6bd44a17fcc0bacac4f9f7956bb07682880344f6
aristrisnawan <aristrisnawan35@gmail.com>
2021-12-23 16:55:30 +0800

    display list surah in card list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
 import CardListComponent from "./components/CardListComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import SeacrhComponent from "./SeacrhComponent";
 
 function App() {
+  const [surah, setSurah] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.quran.sutanlab.id/surah").then((response) => {
+      const listSurah = response.data.data;
+      // console.log(listSurah);
+      setSurah(listSurah);
+    });
+  }, []);
+
+  // console.log(surah);
+
   return (
     <div>
       <HeaderComponent />
       <SeacrhComponent />
-      <CardListComponent />
+      <CardListComponent listsurah={surah} />
     </div>
   );
 }
--- src/components/CardListComponent.js
+++ src/components/CardListComponent.js
@@ -1,36 +1,30 @@
 import React from "react";
 
-export default function CardListComponent() {
+export default function CardListComponent({ listsurah }) {
+  //   console.log(listsurah);
   return (
     <div>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 justify-center mt-6">
-        <div className=" bg-gray-400 w-80 h-32 rounded-md mx-auto mb-4">
-          <div className="mt-6">
-            <p className="font-bold text-xl pl-2">Al-baqarah</p>
-            <p className="font-extrabold text-2xl float-right pr-2">
-              Tulisan Arab
-            </p>
-            <p className="font-semibold mt-5 pl-2">Sapi Betina</p>
-          </div>
-        </div>
-        <div className=" bg-gray-400 w-80 h-32 rounded-md mx-auto mb-4">
-          <div className="mt-6">
-            <p className="font-bold text-xl pl-2">Al-baqarah</p>
-            <p className="font-extrabold text-2xl float-right pr-2">
-              Tulisan Arab
-            </p>
-            <p className="font-semibold mt-5 pl-2">Sapi Betina</p>
-          </div>
-        </div>
-        <div className=" bg-gray-400 w-80 h-32 rounded-md mx-auto mb-4">
-          <div className="mt-6">
-            <p className="font-bold text-xl pl-2">Al-baqarah</p>
-            <p className="font-extrabold text-2xl float-right pr-2">
-              Tulisan Arab
-            </p>
-            <p className="font-semibold mt-5 pl-2">Sapi Betina</p>
-          </div>
-        </div>
+        {listsurah.map((surah) => {
+          return (
+            <div
+              className=" bg-gray-400 w-80 h-32 rounded-md mx-auto mb-4"
+              key={surah.number}
+            >
+              <div className="mt-6">
+                <p className="font-bold text-xl pl-2">
+                  {surah.name.transliteration.id}
+                </p>
+                <p className="font-extrabold text-2xl float-right pr-2">
+                  {surah.name.short}
+                </p>
+                <p className="font-semibold mt-5 pl-2">
+                  {surah.name.translation.id}
+                </p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
